aa489a4f4ffe93714f55d0967e5db7c8033ad355
Maxime Brocourt <maxime.brocourt@etu.unice.fr>
2017-12-27 21:03:57 +0100

    Méthode pour vérifier si on est à portée pour attaquer l'autre robot

--- src/main/java/fr/miage/pav/plugins/Attaque.java
+++ src/main/java/fr/miage/pav/plugins/Attaque.java
@@ -11,8 +11,19 @@ public class Attaque {
         this.me = me;
     }
 
+    /**
+     * Method for detect if the target can be atq by the robot
+     * @param target
+     * @param range
+     * @return
+     */
     public boolean verifRange(Robot target, int range) {
-        return true;
+        int absTarget = target.getX();
+        int ordTarget = target.getY();
+        int absMe = me.getX();
+        int ordMe = me.getY();
+
+        return (Math.abs(absMe - absTarget) <= range) && (Math.abs(ordMe - ordTarget)) <= range;
     }
 
     public void drawAtq(Graphics draw) {
