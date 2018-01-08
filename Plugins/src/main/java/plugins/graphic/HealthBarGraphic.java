6ab45a4da4c35326153e26cef9c428b86518ae87
Louis Prud'homme <louis.prudhomme@hotmail.com>
2018-01-08 14:19:05 +0100

    Fefactor de HealthBarGraphic afin d’en faire un décorateur de BaseGraphic.

--- Plugins/src/main/java/plugins/graphic/HealthBarGraphic.java
+++ Plugins/src/main/java/plugins/graphic/HealthBarGraphic.java
@@ -4,18 +4,19 @@ import identity.IRobot;
 
 import java.awt.*;
 
-public class HealthBarGraphic implements IGraphic{
-    private Color c;
+public class HealthBarGraphic extends BaseGraphic
+{
     private static final int BASE_WIDTH = 10;
-    private BaseGraphic rob;
 
-    public HealthBarGraphic() {
-        this.rob = new BaseGraphic();
+    public HealthBarGraphic()
+    {
+        super();
     }
 
     @Override
-    public void draw(IRobot subject, Graphics g) {
-        rob.draw(subject, g);
+    public void draw(IRobot subject, Graphics g)
+    {
+        super.draw(subject, g);
         g.setColor(Color.GREEN);
         int displayLife = subject.getLife() / 3;
         g.fillRect(subject.getX() - displayLife /2 , subject.getY() + BASE_WIDTH, displayLife , 5);
