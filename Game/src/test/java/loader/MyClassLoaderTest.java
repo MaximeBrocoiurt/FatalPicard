261914ae027bff40a345e441f9d59bf8b65e371a
Louis Prud'homme <louis.prudhomme@hotmail.com>
2018-01-08 14:18:31 +0100

    Refactor de MyClassLoaderTest afin de correspondre à la charte.

--- Game/src/test/java/loader/MyClassLoaderTest.java
+++ Game/src/test/java/loader/MyClassLoaderTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-public class testMyClassLoader {
+public class MyClassLoaderTest {
 
     @Test
     public void testLoadJar(){
