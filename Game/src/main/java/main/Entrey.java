b4938ae94d9e196afeda9314d9e348d92e753b48
Louis Prud'homme <louis.prudhomme@hotmail.com>
2018-01-08 15:59:16 +0100

    Modification d’Entrey pour pouvoir lancer le jeu depuis la ligne de commande.

--- Game/src/main/java/main/Entrey.java
+++ Game/src/main/java/main/Entrey.java
@@ -13,22 +13,24 @@ public class Entrey
 {
     public static void main(String[] args)
     {
-        //On indique où se trouve le dossier contenant les .jar pour y chercher toutes les classes qu'ils faut charger
-        File basPathPlugin = new File(System.getProperty("user.dir") + File.separatorChar + "plugins"+ File.separatorChar + "target");
-        System.out.println("Chemin plugins "+basPathPlugin.getPath().toString());
-        Repository myLoader = new Repository(basPathPlugin);
-
-        //Une fois chargé, elles sont disponible dans cette liste
-        List<Class<?>> myPlugin = myLoader.load();
-
-        for(Class classe : myPlugin){
-            System.out.println("Classes chargées du plugin : "+classe.getName());
-        }
+//        //On indique où se trouve le dossier contenant les .jar pour y chercher toutes les classes qu'ils faut charger
+//        File basPathPlugin = new File(System.getProperty("user.dir") + File.separatorChar + "plugins"+ File.separatorChar + "target");
+//        System.out.println("Chemin plugins " + basPathPlugin.getPath());
+//        Repository myLoader = new Repository(basPathPlugin);
+//
+//        //Une fois chargé, elles sont disponible dans cette liste
+//        List<Class<?>> myPlugin = myLoader.load();
+//        System.out.println(myPlugin);
+//        for(Class classe : myPlugin){
+//            System.out.println("Classes chargées du plugin : "+classe.getName());
+//        }
 
         JFrame f = new JFrame("RobotWar");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        War w = new War(500, 500, 10);
+
+
+        War w = new War(500, 500, Integer.valueOf(args[0]));
         f.getContentPane().add(w, BorderLayout.CENTER);
         w.launch();
 
