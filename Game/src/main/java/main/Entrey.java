6286cb72421d15fb8129e80d9a0f80b06a5e5cc2
Maxime Brocourt <maxime.brocourt@etu.unice.fr>
2018-01-09 13:58:55 +0100

    Ajout d'un menu de navigation avec méthode d'enrichissement et d'un bouton start

--- Game/src/main/java/main/Entrey.java
+++ Game/src/main/java/main/Entrey.java
@@ -5,6 +5,8 @@ import loader.PluginLoader;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.List;
 
@@ -26,14 +28,63 @@ public class Entrey
 
         JFrame f = new JFrame("RobotWar");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton start = new JButton();
+        start.setText("Start Game");
 
+        JMenuBar menu = new JMenuBar();
+        JMenu menuAttack = new JMenu("IAttack");
+        JMenu menuMove = new JMenu("IMove");
+        JMenu menuGraphique = new JMenu("IGraphique");
 
+        menu.add(menuAttack);
+        menu.add(menuMove);
+        menu.add(menuGraphique);
+
+        //TODO: item à mettre dans le menu avec les classes chargé
+        addItemMenu(menuAttack);
+        addListener(menuAttack);
+
+        f.getContentPane().add(menu, BorderLayout.NORTH);
 
         War w = new War(500, 500, Integer.valueOf(args[0]));
         f.getContentPane().add(w, BorderLayout.CENTER);
+        f.getContentPane().add(start, BorderLayout.SOUTH);
         w.launch();
 
         f.pack();
         f.setVisible(true);
+    }
+
+    /**
+     * Method for add item in menu passed in parameters
+     * @param menu
+     */
+    private static void addItemMenu(JMenu menu) {
+        JMenuItem newMenu = new JMenuItem("test");
+        menu.add(newMenu);
+    }
+
+    /**
+     * method use for add listener for tab type
+     * @param tab
+     */
+    private static void addListener(JMenu tab) {
+        switch (tab.getText()) {
+            case "IAttack":
+                for (int i=0; i<tab.getItemCount(); i++) {
+                    JMenuItem item = tab.getItem(i);
+                    item.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            System.out.println("Evenement de base");
+                        }
+                    });
+                }
+                break;
+            case "IMove":
+                break;
+            case "IGraphique":
+                break;
+        }
     }
 }
