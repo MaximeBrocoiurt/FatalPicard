354dae4c47da349b2e509b15304f481c1abd23a8
Louis Prud'homme <louis.prudhomme@hotmail.com>
2018-01-11 11:22:52 +0100

    ajout de phœnixAttack

---
+++ Plugins/OptionnalPlugins/src/main/java/plugins/attack/PhoenixAttack.java
@@ -0,0 +1,57 @@
+package plugins.attack;
+
+import annotations.Attack;
+import annotations.Plugin;
+import engine.NotEnoughEnergyException;
+import engine.NotInRangeException;
+import identity.IRobot;
+
+@Plugin(type = Plugin.Type.ATTACK)
+public class PhoenixAttack
+{
+    private final int ENERGY_CONSUMPTION = 20;
+    private final int RANGE = 50;
+    private final int POWER = 20;
+
+    /**
+     * Implémentation de attack
+     * @param target cible à attaquer
+     * @throws NotEnoughEnergyException lorsqu’il n’y a pas assez d’énergie pour attaquer.
+     * @throws NotInRangeException lorsque le robot ennemi n’est pas à portée.
+     */
+    @Attack(nature = Attack.Nature.MAIN)
+    public void attack(IRobot subject, IRobot target) throws NotEnoughEnergyException, NotInRangeException
+    {
+        if(subject.getEnergy() >= ENERGY_CONSUMPTION)
+        {
+            if(subject.calculateDistance(target) <= RANGE)
+            {
+                target.decreaseLife(POWER);
+                subject.decreaseEnergy(ENERGY_CONSUMPTION);
+                subject.decreaseLife(POWER);
+            } else {
+                throw new NotInRangeException();
+            }
+        } else {
+            throw new NotEnoughEnergyException();
+        }
+    }
+
+    @Attack(nature = Attack.Nature.POWER)
+    public int getPower()
+    {
+        return this.POWER;
+    }
+
+    @Attack(nature = Attack.Nature.RANGE)
+    public int getRange()
+    {
+        return this.RANGE;
+    }
+
+    @Attack(nature = Attack.Nature.CONSUMPTION)
+    public int getConsumption()
+    {
+        return this.ENERGY_CONSUMPTION;
+    }
+}
